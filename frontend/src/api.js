52a12740711dc9d60424f5a6ddf3d1adca7593ca
Abdul Momin Alam <momin.alam@sabanciuniv.edu>
2026-07-27 15:02:34 +0500

    added some npm packages and fixed api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
-    Authorization: "Bearer fda40cf7ace9ab49144708d3e4720be062fa92d0f26f1850064f7420678bd047",
+    Authorization: `Bearer ${import.meta.env.VITE_API_TOKEN}`,
+    "Content-Type": "application/json",
   },
 });
 
